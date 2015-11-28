3af2aaddfea11326c28d76ac2ba9e7fe47a71c9e
Sophia Kim <sjk7fu@virginia.edu>
2015-11-27 22:58:28 -0500

    add brighten and darken functions to imagine.py

---
+++ code/python/imageproc/image_processing.py
@@ -0,0 +1,119 @@
+import pygame as pg
+
+################### PIXEL OPERATIONS ######################
+
+# grayPixel: pixel -> pixel
+# compute and return a gray pixel with the same intensity
+# as the given pixel. A little complication: the intensity
+# values in the 3D numpy array representing the pixels are
+# expressed as 8-bit integers. The range is 0..255. The
+# problem is that if one adds together three of these, to
+# get aonther 8-bit integer, there can be an overflow! So
+# we convert the 8-bit integers to values of type "int" before
+# we do the addition. The "int" type in Python can hold
+# integer values of any size.
+#
+def grayPixel(pixel):
+    red_intensity = int(pixel[0])
+    green_intensity = int(pixel[1])
+    blue_intensity = int(pixel[2])
+    ave_intensity = (red_intensity + green_intensity + blue_intensity)//3
+    
+    return ((ave_intensity, ave_intensity, ave_intensity))
+
+# channel: pixel -> channel -> pixel
+# return a gray pixel with intensity from given channel of given pixel
+def channel(pixel,chan):
+    return (pixel[chan],pixel[chan],pixel[chan])
+
+
+# inverse: pixel -> pixel
+# return the color negative of the given pixel
+def inverse(pixel):
+    return (255-pixel[0], 255-pixel[1], 255-pixel[2])
+
+
+# intensify: pixel -> nat255 -> pixel
+# brighten each channel of pixel by quantity
+#
+# NOTE: there might be an overflow bug in this code!
+# If so, reason through it and fix it. Consider the
+# possible need for a function that brightens an
+# individual pixel intensity, never returning a result
+# greater than 255 or less than 0.
+#
+def intensify(pixel):
+    if (pixel[0] <= 245 and pixel[1] <= 245 and pixel[2] <= 245):
+        return (pixel[0]+10, pixel[1]+10, pixel[2]+10)
+    else:
+        return (pixel[0], pixel[1], pixel[2])
+
+def darken(pixel):
+    if (pixel[0] >= 10 and pixel[1] >= 10 and pixel[2] >= 10):
+        return (pixel[0]-10, pixel[1]-10, pixel[2]-10)
+    else:
+        return (pixel[0], pixel[1], pixel[2])
+################### IMAGE OPERATIONS ######################
+
+# invert: modifies image pixel array of image_surf in place
+# replace each pixel with its photographic "negative"
+#
+def invert(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = inverse(pixels3d[x,y])
+
+
+
+# bw: modifies image pixel array of image_surf in place
+# replaces each pixel with a corresponding gray-scale pixel
+def bw(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = grayPixel(pixels3d[x,y])
+
+def bri(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = intensify(pixels3d[x,y])
+
+def dark(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = darken(pixels3d[x,y])
